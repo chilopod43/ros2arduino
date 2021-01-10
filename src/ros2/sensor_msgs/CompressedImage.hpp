ebb20793975d72ddf9bc1a1a8c8973073da87b3d
chilopod43 <inkstone.mukade@gmail.com>
2021-01-11 02:34:46 +0900

    revert data_array_capacity

--- src/ros2/sensor_msgs/CompressedImage.hpp
+++ src/ros2/sensor_msgs/CompressedImage.hpp
@@ -33,14 +33,15 @@ namespace sensor_msgs
     public:
         std_msgs::Header header;
         char format[255];
-        uint8_t data[10000];
+        uint8_t *data;
         uint32_t data_size;
+        uint32_t data_array_capacity;
 
         CompressedImage() : Topic("sensor_msgs::msg::dds_::CompressedImage_", "CompressedImage", SENSOR_MSGS_COMPRESSED_IMAGE_ID),
-                            header(), data_size(0)
+                            header(),
+                            data(nullptr), data_size(0), data_array_capacity(0)
         {
             memset(format, 0, sizeof(format));
-            memset(data, 0, sizeof(data));
         }
 
         bool serialize(void *msg_buf, const CompressedImage *topic)
@@ -58,7 +59,7 @@ namespace sensor_msgs
             ucdrBuffer *reader = (ucdrBuffer *)msg_buf;
             (void)header.deserialize(reader, &topic->header);
             (void)ucdr_deserialize_string(reader, topic->format, sizeof(topic->format));
-            (void)ucdr_deserialize_sequence_uint8_t(reader, topic->data, 10000, &topic->data_size);
+            (void)ucdr_deserialize_sequence_uint8_t(reader, topic->data, topic->data_array_capacity, &topic->data_size);
 
             return !reader->error;
         }
